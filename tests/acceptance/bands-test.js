ac1dcf7388e2b2ed4f10ac8d0f8ac9e2eb074b9e
Bruno <bruno@garciaechegaray.com>
2016-01-06 18:19:02 +0100

    Chapter 11: Testing

--- tests/acceptance/bands-test.js
+++ tests/acceptance/bands-test.js
@@ -51,3 +51,55 @@ test('List bands', function(assert) {
     assert.equal(find('.band-link:contains("Long Distance Calling")').length, 1, "The other band link contains the band name");
   });
 });
+
+test('Create a new song in two steps', function(assert) {
+
+  server = new Pretender(function() {
+
+    this.get('/bands', function() {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: "bands",
+            attributes: {
+              name: "Radiohead"
+            }
+          }
+        ]
+      };
+      return [200, { "Content-Type": "application/vnd.api+json" },
+      JSON.stringify(response)];
+    });
+
+    this.post('/songs', function() {
+      var response = {
+        data: [
+          {
+            id: 1,
+            type: "songs",
+            attributes: {
+              name: "Killer Cars"
+            }
+          }
+        ]
+      };
+      return [200, { "Content-Type": "application/vnd.api+json" },
+      JSON.stringify(response)];
+    });
+
+    this.get('/bands/1/songs', () => {
+      return [200, { "Content-Type": "application/vnd.api+json" },
+      JSON.stringify({ data: [] })];
+    });
+  });
+
+  visit('/');
+  click('.band-link:contains("Radiohead")');
+  click('a:contains("create one")');
+  fillIn('.new-song', 'Killer Cars');
+  triggerEvent('.new-song-form', 'submit');
+  andThen(function() {
+    assert.equal(find('.songs .song:contains("Killer Cars")').length,1, "Creates the song and displays it in the list");
+  });
+});
